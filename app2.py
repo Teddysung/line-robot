8a26d07d3a86b8b5169f0c13527c79d7db3474af
Teddy Sung <teddysung168@gmail.com>
2019-09-01 17:41:21 +0800

    the first dinner chooser

--- app2.py
+++ app2.py
@@ -10,6 +10,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import random
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('27xL1uDr5z7jHkjiXYXQmDbCW4pj6xYEKLEpa8DW72xmOBa5htCEVUgCBFpjvj5VDuljhLqVizFiv24swJcAmcGvTixVff++g8dZBvAylXflp2taMBxr96+jTqeKI5lGQ7Ln0AXdZqYKwviZ81SUtgdB04t89/1O/w1cDnyilFU=')
@@ -37,10 +39,21 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    r = random.randint(1, 2)
     msg = event.message.text
+    if msg == '今天要吃什麼':
+        s = '等我一下'
+        if r == 1:
+            s = '今天吃豬'
+        elif r == 2:
+            s = '今天吃魚'
+    elif meg == '謝謝你':
+        s = '不客氣'
+    else:
+        s = '滾' 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='哈囉'))
+        TextSendMessage(text=s))
 
 
 if __name__ == "__main__":
