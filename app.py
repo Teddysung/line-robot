82bfd59452ddd109c88235f4d0efdedac1bb0805
Teddy Sung <teddysung168@gmail.com>
2019-09-01 17:24:39 +0800

    test3-1

--- app.py
+++ app.py
@@ -38,16 +38,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '謝謝再聯絡'
+    # s = '謝謝再聯絡'
 
-    if msg == '你好':
-        s = '滾'
-    elif msg == '你過的好嗎':
-        s = '我要睡覺'
+    # if msg == '你好':
+    #     s = '滾'
+    # elif msg == '你過的好嗎':
+    #     s = '我要睡覺'
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=msg))
 
 
 if __name__ == "__main__":
